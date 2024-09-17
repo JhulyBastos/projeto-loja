88c480d2a11ac7abf09f979e131e797f1e4ba57b
Jhuly Bastos <jhulytbds@gmail.com>
2024-09-17 11:26:35 -0300

    input styling

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -6,14 +6,14 @@ interface InputProps extends ComponentProps<"input"> {
 
 export function Input({ variant = "primary", ...props }: InputProps) {
   const variants = {
-    primary: "",
-    secondary: "w-[180px]",
-    tertiary: "w-[170px] rounded-l-lg border-y border-l",
+    primary: "rounded border",
+    secondary: "w-[180px] rounded border",
+    tertiary: "w-[210px] rounded-l-lg border-y border-l",
   };
   return (
     <input
       {...props}
-      className={`${variants[variant]} bg-dark-30 w-full  h-10 p-4 rounded border border-black/5 text-black focus:outline-none placeholder:text-sm`}
+      className={`${variants[variant]} bg-dark-30 w-[250px]  h-10 p-4  border-black/5 text-black focus:outline-none placeholder:text-sm`}
     />
   );
 }
--- src/components/ShippingDetails/index.tsx
+++ src/components/ShippingDetails/index.tsx
@@ -11,27 +11,31 @@ export default function ShippingDetails() {
         <div>
           <h1 className="text-black text-xl py-7">Detalhes do envio</h1>
           <div className="flex flex-col  gap-1">
-            <div className="flex justify-between gap-5">
+            <div className="flex justify-between">
               <div>
                 <p className="text-black text-sm">Estado</p>
                 <div className="flex items-center">
                   <Input variant="tertiary" placeholder="Estado" className="" />
                   <div className="w-[40px] h-[40px] bg-dark-30 flex items-center justify-center rounded-r-lg border-y border-r border-black/5">
-                    <ChevronDown color="black" className="w-5 " />
+                    <button>
+                      <ChevronDown color="grey" className="w-5 " />
+                    </button>
                   </div>
                 </div>
               </div>
               <div>
                 <p className="text-black text-sm">Cidade</p>
+
                 <div className="flex items-center">
                   <Input variant="tertiary" placeholder="Cidade" className="" />
                   <div className="w-[40px] h-[40px] bg-dark-30 flex items-center justify-center rounded-r-lg border-y border-r  border-black/5">
-                    <ChevronDown color="black" className="w-5" />
+                    {" "}
+                    <ChevronDown color="grey" className="w-5" />
                   </div>
                 </div>
               </div>
             </div>
-            <div className="flex justify-between">
+            <div className="flex gap-5">
               <div>
                 <p className="text-black text-sm">Rua</p>
                 <Input placeholder="Rua" className="" />
@@ -41,7 +45,7 @@ export default function ShippingDetails() {
                 <Input variant="secondary" placeholder="Número" className="" />
               </div>
             </div>
-            <div className="flex justify-between">
+            <div className="flex gap-5">
               <div>
                 <p className="text-black text-sm">Bairro</p>
                 <Input placeholder="Bairro" className="" />
